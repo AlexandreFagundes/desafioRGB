0a7926583466355bd0467271283102b422347e0d
alfagundes <36684258+alfagundes@users.noreply.github.com>
2020-07-18 18:15:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', init);
+
+var rangeRed = document.querySelector('#rangeRed');
+var rangeGreen = document.querySelector('#rangeGreen');
+var rangeBlue = document.querySelector('#rangeBlue');
+var colorR = document.querySelector('#colorR');
+var colorG = document.querySelector('#colorG');
+var colorB = document.querySelector('#colorB');
+var swapColor = document.querySelector('.swapColor');
+
+rangeRed.addEventListener('input', setColor);
+rangeGreen.addEventListener('input', setColor);
+rangeBlue.addEventListener('input', setColor);
+
+function init() {
+  console.log("Carrega Página.");
+  setColor();
+}
+
+function setColor() {
+  colorR.value = rangeRed.value;
+  colorG.value = rangeGreen.value;
+  colorB.value = rangeBlue.value;
+  //swapColor.style.backgroundColor = 'rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})';
+  swapColor.setAttribute('style', 'background-color: rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})');
+}
