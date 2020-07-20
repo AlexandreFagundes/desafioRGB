1fafd61d72ac7d8a3efa776d1892e04de71b70d8
alfagundes <alexandre.fagundes@live.com>
2020-07-20 17:09:26 -0300

    Atualizacao de projeto.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ var colorR = document.querySelector('#colorR');
 var colorG = document.querySelector('#colorG');
 var colorB = document.querySelector('#colorB');
 var swapColor = document.querySelector('.swapColor');
+//const swapColorHex = document.querySelector('#swapColorHex > p');
+
 
 rangeRed.addEventListener('input', setColor);
 rangeGreen.addEventListener('input', setColor);
@@ -21,6 +23,6 @@ function setColor() {
   colorR.value = rangeRed.value;
   colorG.value = rangeGreen.value;
   colorB.value = rangeBlue.value;
-  //swapColor.style.backgroundColor = 'rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})';
-  swapColor.setAttribute('style', 'background-color: rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})');
+  swapColor.setAttribute('style', `background-color: rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})`);
+  //swapColorHex.innerHTML = swapColor;
 }
